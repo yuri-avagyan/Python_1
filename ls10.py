76688043402bbfc0c69ac2c88b80b92252b8c36e
Avagyan7 <yuri.avagyan1995@gmail.com>
2021-06-26 16:29:08 +0400

    new file is added

---
+++ ls10.py
@@ -0,0 +1,45 @@
+
+#task 1
+
+# dict1 = {1:10, 2:20}
+# dict2 = {3:30, 4:40}
+# dict3 = {5:50, 6:60}
+# dict4 = {6:50, 7:60}
+# new_dict = {}
+
+# new_dict.update(dict1)
+# new_dict.update(dict2)
+# new_dict.update(dict3)
+# new_dict.update(dict4)
+
+
+# print(new_dict)
+
+#task 2
+
+# dict_1 = {'c1': 'Red', 'c2': 'Green', 'c3': None}
+# dict_2 = {}
+
+# for key, value in dict_1.items():
+# 	if value != None:
+# 		dict_2[key] = dict_1[key]
+
+# print(dict_2)
+
+#task 3
+
+# n = int(input("input a lenght of dict\n"))
+# dict_1 = {1:1}
+# dict_2 = {}
+
+# for i in range(1, n + 1):
+# 	dict_1[1] * i 
+# 	dict_2[i] = dict_1[1]
+# 	dict_2[i] *= i
+# 	dict_2[i] *= i 
+
+
+# 	print(dict_2)
+
+
+
